025e15eacfab18a4ea5c486e00fadf8aa182132a
Sudhir Dhameliya <sudhirrd007@gmail.com>
2021-11-19 17:46:26 -0500

    Create projects.js

---
+++ public/javascripts/projects.js
@@ -0,0 +1,9 @@
+// when mouse pointer enters inside any project blobk
+function on() {
+    document.getElementById("overlay").style.display = "block";
+}
+
+// when mouse pointer exits any project blobk
+function off() {
+    document.getElementById("overlay").style.display = "none";
+}
