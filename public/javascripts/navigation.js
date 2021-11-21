1a84b6e1e11929f751c7a217ebe09effb0206008
Sudhir Dhameliya <sudhirrd007@gmail.com>
2021-11-21 17:19:45 -0500

    Update navigation.js

--- public/javascripts/navigation.js
+++ public/javascripts/navigation.js
@@ -1,5 +1,5 @@
 // to navigate between pages
-function goto() {
+function goto(page) {
     // get current page address
     var segments = window.location.href.split("/");
     var baseUrl = String(segments.slice(0,3).join("/"));
